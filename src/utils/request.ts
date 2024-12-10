2838fec2b90e2b609bd965a714914f83cd38eb8e
wanzigou1 <745737762@qq.com>
2024-12-10 22:30:35 +0800

    update

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -34,7 +34,7 @@ request.interceptors.response.use(
       message.error("error");
     }
     // 对响应数据进行处理
-    return response.data?.result || response.data; // 这里直接返回响应的数据部分
+    return response.data; // 这里直接返回响应的数据部分
   },
   (error) => {
     console.log(error);
